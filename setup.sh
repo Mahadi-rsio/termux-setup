67ea83c4573277853ed4b06e32647afde5e81256
xio <rsioex@gmail.com>
2024-10-15 15:43:51 +0600

    --

--- setup.sh
+++ setup.sh
@@ -1,5 +1,41 @@
-pkg update -y
-pkg upgrade -y
-pkg install root-repo -y
-pkg install x11-repo -y
+
+echo "Mahadi @RSIO v.0.1"
+
+sleep 0.5
+
+echo "[Git] Github: https://www.github.com/Mahadi-rsio"
+
+sleep 0.5
+
+echo "[WARN] This Project is for personal uses only"
+
+sleep 0.5
+
+echo "[WARN] Some issue will found while using"
+
+sleep 0.5
+
+echo "[WARN] Some functionality may not works properly "
+
+sleep 0.5
+
+echo "[INFO] Checking System"
+
+sleep 0.5
+
+echo "[INFO] System ready for installation"
+
+clear
+
+rm -rf termux-setup.zip
+
+
+
+#pkg update -y
+#pkg upgrade -y
+#pkg install root-repo -y
+#pkg install x11-repo -y
+#pkg install tur-repo -y
+#pkg install tree -y
+
 
